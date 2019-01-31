247947a4e2f0c19653fd65809ff2c341d006b99a
Anna Sedlar <annasedlar@gmail.com>
2019-01-31 17:59:09 -0500

    put retrofit into own repository

--- app/src/main/java/com/bignerdranch/android/flickrphotogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/flickrphotogallery/PhotoGalleryFragment.kt
@@ -6,13 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -23,27 +20,12 @@ class PhotoGalleryFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
 
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.flickr.com/")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build()
-
-        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
-
-        val flickrHomePageReqest: Call<String> = flickrApi.fetchContents()
-
-        flickrHomePageReqest.enqueue(object : Callback<String> {
-            override fun onFailure(call: Call<String>, t: Throwable) {
-                Log.e(TAG, "Failed to fetch photos", t)
-            }
-
-            override fun onResponse(
-                call: Call<String>,
-                response: Response<String>
-            ) {
-                Log.d(TAG, "Response received: ${response.body()}")
-            }
-        })
+        val flickrLiveData: LiveData<String> = FlickrFetchr().fetchContents()
+        flickrLiveData.observe(
+            viewLifecycleOwner,
+            Observer { responseString ->
+                Log.d(TAG, "response received: $responseString")
+            })
 
         photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
         photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
